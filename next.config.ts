8134fd4465704304ca7cf0570390adbd9584b240
Cminchala <christianminchala97@gmail.com>
2025-02-26 11:39:06 -0500

    pointed domain / to the static website index.html

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-}
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/relianceautoworks.com/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
